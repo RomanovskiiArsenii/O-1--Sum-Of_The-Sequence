46c4f1356a55cc964f579c7dd18eb9b851643941
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-07-29 07:13:01 +0400

    Update Program.cs

--- O(1)-Sum-Of_The-Sequence/Program.cs
+++ O(1)-Sum-Of_The-Sequence/Program.cs
@@ -3,13 +3,16 @@ using System.Numerics;
 
 
 //We need to calculate the sum of all numbers through 1 to N inclusively
+
 //As we can see for number 6
 //1+2+3+4+5+6 = 21
 //Equal to:
 //(1+6)+(2+5)+(3+4)= 7×3 = 21
-//And for
+
+//And for number 7:
 //1+2+3+4+5+6+7 = 28
 //(1+6)+(2+5)+(3+4)+7 = 7*4 = 28
+
 //Based on this information, we can write a method that quickly sums a sequence of numbers.
 //The code presents a regular loop for comparison and a method that calculates the sum of
 //the entire sequence of digits from 1 to N in three operations (bitwise shift, incrementation,
